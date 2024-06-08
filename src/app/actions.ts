b8184a5fae69de60f59d6f62658696e9deeb7324
jaggerharness <jagger.dev@gmail.com>
2024-06-08 00:39:53 -0500

    feat: on register generate jwt token for verification / need callback

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,6 +4,8 @@ import { SES } from '@aws-sdk/client-ses';
 import { PrismaClient } from '@prisma/client';
 import { render } from '@react-email/render';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { JWT } from 'next-auth/jwt';
 import EmailVerificationEmail from '../../emails/email-verification';
 
 export async function registerUser({ formData }: { formData: FormData }) {
@@ -22,6 +24,35 @@ export async function registerUser({ formData }: { formData: FormData }) {
       emailVerified: new Date(),
     },
   });
+
+  const jwtPayload: JWT = {
+    id: user.id,
+    email: user.email,
+  };
+
+  const jwtSecret = process.env.AUTH_SECRET;
+  if (!jwtSecret) {
+    throw new Error('JWT secret is not set');
+  }
+
+  const token = jwt.sign(jwtPayload, jwtSecret, { expiresIn: '1h' });
+
+  console.log(token);
+
+  const validToken = await new Promise<boolean>((resolve) => {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        resolve(false);
+      } else {
+        const jwtPayload = decoded as JWT;
+        console.log(jwtPayload.id);
+        console.log(jwtPayload.email);
+        resolve(true);
+      }
+    });
+  });
+
+  console.log(validToken);
 
   const region = process.env.AWS_REGION;
   const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
