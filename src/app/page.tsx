dd60f4624b2a755780aad5514303eebce81dad6b
jaggerharness <jagger.dev@gmail.com>
2024-10-10 22:15:19 -0500

    chore: remove beta testing warning - next steps: testing / logging / analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Alert, AlertDescription } from '@/components/shad-ui/alert';
 import { auth } from '@/lib/auth';
-import { AlertCircle } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import LoginCard from './components/login-card';
 
@@ -13,12 +11,6 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col h-screen items-center justify-center gap-8">
-      <Alert variant="default" className="w-3/4 md:w-1/4">
-        <AlertCircle className="size-4" />
-        <AlertDescription>
-          Beta testing is currently taking place. Data is reset frequently. Report any issues to support@liftlens.app
-        </AlertDescription>
-      </Alert>
       <LoginCard />
     </main>
   );
