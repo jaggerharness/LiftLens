b16f3a2b5635f845711bef5654a0ba9c3c35191b
jaggerharness <jagger.dev@gmail.com>
2024-10-28 22:14:13 -0500

    feat: analytics - security headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,31 @@ const config = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff'
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY'
+              },
+              {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block'
+              },
+              {
+                key: 'Permissions-Policy',
+                value: 'interest-cohort=(), geolocation=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), payment=(), usb=(), vr=(), accelerometer=()'
+              }
+            ],
+          },
+        ]
+      },
 };
 
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import { ThemeProvider } from '@/components/shad-ui/theme-provider';
-import { Toaster } from '@/components/shad-ui/toaster';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import React from 'react';
+import { ThemeProvider } from "@/components/shad-ui/theme-provider";
+import { Toaster } from "@/components/shad-ui/toaster";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'LiftLens',
+  title: "LiftLens",
   description:
-    'Workout tracking application which provides basic workout tracking, planning, and insights into the muscle groups targeted by the provided workouts.',
+    "Workout tracking application which provides basic workout tracking, planning, and insights into the muscle groups targeted by the provided workouts.",
 };
 
 export default function RootLayout({
@@ -28,6 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
